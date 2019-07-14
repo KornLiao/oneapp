ad225d6e187f612c80dea4284dccce76405de586
Korn <skycorn12@gmail.com>
2019-07-14 22:05:18 +0800

    Initial Commit

---
+++ oneapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  oneapp
+//
+//  Created by 廖昱銘 on 2019/7/9.
+//  Copyright © 2019 廖昱銘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
